c7b59f96294ab3375729a6594818c0ff92179821
lexskiies <alexis.cheyy@gmail.com>
2017-12-21 08:13:08 +0100

    servlet + bean + jsp inscription

--- TPsiteMarchand/src/java/tp/servlets/CreationClient.java
+++ TPsiteMarchand/src/java/tp/servlets/CreationClient.java
@@ -6,81 +6,52 @@
 package tp.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import tp.beans.Client;
 /**
  *
  * @author alexi
  */
 public class CreationClient extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CreationClient</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CreationClient at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    @Override
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /*
+         * Récupération des données saisies, envoyées en tant que paramètres de
+         * la requête GET générée à la validation du formulaire
+         */
+        String nom = request.getParameter( "nom" );
+        String prenom = request.getParameter( "prenom" );
+        String adresse = request.getParameter( "adresse" );
+        String telephone = request.getParameter( "telephone" );
+        String email = request.getParameter( "adrMail" );
+        String mdp = request.getParameter("mdp");
+        String message;
+
+        if ( nom.trim().isEmpty() || adresse.trim().isEmpty() || telephone.trim().isEmpty() ) {
+            message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"creerClient.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'un client.";
+        } else {
+            message = "Client créé avec succès !";
         }
+        
+
+        Client client = new Client();
+        client.setNom( nom );
+        client.setPrenom( prenom );
+        client.setAdresse( adresse );
+        client.setTelephone( telephone );
+        client.setEmail( email );
+        client.setMdp(mdp);
+
+        /* Ajout du bean et du message à l'objet requête */
+        request.setAttribute( "client", client );
+        request.setAttribute( "message", message );
+
+        /* Transmission à la page JSP en charge de l'affichage des données */
+        this.getServletContext().getRequestDispatcher( "/afficherClient.jsp" ).forward( request, response );
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
--- TPsiteMarchand/web/inscription.jsp
+++ TPsiteMarchand/web/inscription.jsp
@@ -12,19 +12,20 @@
         <title>s'inscrire</title>
     </head>
     <body>
-        <form>
+        <form method="get" action="creationClient">
             <label for="adrmail">Adresse e-mail : </label>
-            <input type="email" name="adrMail">
+            <input type="email" name="adrMail"></br>
             <label for="mdp">Mot de passe : </label>
-            <input type="password" name="mdp">
+            <input type="password" name="mdp"></br>
             <label for="nom">Nom : </label>
-            <input type="text" name="nom">
+            <input type="text" name="nom"></br>
             <label for="prenom">Prenom : </label>
-            <input type="text" name="prenom">
+            <input type="text" name="prenom"></br>
             <label for="adresse">Adresse de livraison : </label>
-            <input type="text" name="adresse">
+            <input type="text" name="adresse"></br>
             <label for="telephone">N° de téléphone : </label>
-            <input type="tel" name="telephone">
+            <input type="tel" name="telephone"></br>
+            <input type="submit" name="valider" value="S'inscrire">
         </form>
     </body>
 </html>
